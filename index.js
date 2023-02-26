ad0d67d6f8f51c58aa17e8bdedd5eef4928b5637
Arafat447 <arafathossain05541@gmail.com>
2023-02-26 14:13:19 +0600

    change for test

--- index.js
+++ index.js
@@ -1,3 +1,116 @@
+// const express = require("express");
+// const cors = require("cors")
+// const bodyPerser = require("body-parser");
+// const { MongoClient,ObjectId } = require('mongodb');
+// const app = express();
+// const port = 3000;
+// require("dotenv").config();
+// app.use(cors());
+// app.use(bodyPerser.json());
+// // db connection 
+
+// const uri = "mongodb+srv://MyDbUser:gorurhat@cluster0.bssu4.mongodb.net/cowdb?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+// client.connect(err => {
+//   const cowCollection = client.db("cowdb").collection("cows");
+//   const serviceCollection = client.db("cowdb").collection("services");
+//   // Post
+//   app.post("/addCow",(req,res)=>{
+//       const cow = req.body;
+//       cowCollection.insertOne(cow)
+//       .then(result=>{
+//         res.send(result)
+//       })
+//   });
+//   app.post("/addService",(req,res)=>{
+//       const service = req.body;
+//       serviceCollection.insertOne(service)
+//       .then(result=>{
+//         console.log(result)
+//         res.send("success")
+//       })
+//       console.log(service)
+//   });
+//   // Get
+//   app.get("/allCows",(req,res)=>{
+//     cowCollection.find({})
+//       .toArray((err,data)=>{
+//           res.send(data)
+//       })    
+//   });
+//   // cow get by id
+//   app.get("/allCow/:id",(req,res)=>{
+//     cowCollection.find({_id:ObjectId(req.params.id)})
+//       .toArray((err,data)=>{
+//         res.send(data)
+//       })
+//   })
+//   app.get("/allServices",(req,res)=>{
+//     serviceCollection.find({})
+//       .toArray((err,data)=>{
+//           res.send(data)
+//       })    
+//   });
+//   // get by filter
+//   app.get("/allCows/:categori",(req,res)=>{
+//     cowCollection.find({categori:req.params.categori})
+//     .toArray((err,data)=>{
+//       res.send(data)
+//     })
+//   })
+//   // Update 
+//   app.patch("/updateService/:id",(req,res)=>{
+//     serviceCollection.updateOne({_id:ObjectId(req.params.id)},
+//       {
+//         $set:{
+//           description:req.body.description,
+//           serial:req.params.serial,
+//           title:req.params.title
+//         }
+//       })
+//       .then(result=>res.send("Succesfully updated"))
+//   })
+//   app.patch("/updateCowInfo/:id",(req,res)=>{
+//     cowCollection.updateOne({_id:ObjectId(req.params.id)},
+//       {
+//         $set:{
+//           video:req.body.video,
+//           code:req.body.code,
+//           categori:req.body.categori,
+//           address:req.body.address,
+//           type:req.body.type,
+//           age:req.body.age,
+//           teeth:req.body.teeth,
+//           owner:req.body.owner,
+//           price:req.body.price,
+//           thana:req.body.thana,
+//           weight:req.body.weight,
+//           height:req.body.height,
+//           contact:req.body.contact
+//         }
+//       })
+//       .then(result=>console.log(result.modifiedCount))
+//   })
+//   //Delete
+//   app.delete("/deleteCow/:id",(req,res)=>{
+//     cowCollection.deleteOne({_id:ObjectId(req.params.id)})
+//       .then(result=>res.send("Succesfully deleted"))
+//   })
+//   app.delete("/deleteService/:id",(req,res)=>{
+//     serviceCollection.deleteOne({_id:ObjectId(req.params.id)})
+//       .then(result=>res.send("Succesfully deleted"))
+//   })
+// });
+
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
+
+
+// app.listen(process.env.PORT || port, () => {
+//   console.log(`app listening at http://localhost:${port}`)
+// })
 const express = require("express");
 const cors = require("cors")
 const bodyPerser = require("body-parser");
@@ -9,11 +122,11 @@ app.use(cors());
 app.use(bodyPerser.json());
 // db connection 
 
-const uri = "mongodb+srv://MyDbUser:gorurhat@cluster0.bssu4.mongodb.net/cowdb?retryWrites=true&w=majority";
+const uri = "mongodb+srv://morshedzaimahtech:FbU2ZrZx3w03CbiC@cluster0.4p9fior.mongodb.net/tdabDB?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
-  const cowCollection = client.db("cowdb").collection("cows");
-  const serviceCollection = client.db("cowdb").collection("services");
+  const cowCollection = client.db("tdabDB").collection("board-of-directors");
+  const serviceCollection = client.db("tdabDB").collection("services");
   // Post
   app.post("/addCow",(req,res)=>{
       const cow = req.body;
@@ -32,7 +145,7 @@ client.connect(err => {
       console.log(service)
   });
   // Get
-  app.get("/allCows",(req,res)=>{
+  app.get("/board-of-directors",(req,res)=>{
     cowCollection.find({})
       .toArray((err,data)=>{
           res.send(data)
